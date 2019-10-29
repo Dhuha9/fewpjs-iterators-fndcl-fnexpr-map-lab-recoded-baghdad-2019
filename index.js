4b6639ee39042f00115e22553cbcbc1ab5731caa
Dhuha Hasan <duha_9@yahoo.com>
2019-10-29 08:25:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,15 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  let splitStrArray = str.toLowerCase().split(' ');
-   for (var i = 0; i < splitStr.length; i++) {
-       // You do not need to check if i is larger than splitStr length, as your for does that for you
-       // Assign it back to the array
-       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+  let splitStrArray = tutorials.toLowerCase().split(' ');
+   for (let i = 0; i < splitStrArray.length; i++) {
+       splitStrArray[i] = splitStrArray[i].charAt(0).toUpperCase() + splitStrArray[i].substring(1);     
    }
-   // Directly return the joined string
-   return splitStr.join(' '); 
-}
-
+   tutorials= splitStrArray.join(' '); 
   return tutorials
 }
