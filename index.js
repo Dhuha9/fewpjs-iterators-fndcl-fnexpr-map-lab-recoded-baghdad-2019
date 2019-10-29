cda0dc25998f1e02f77678372d538fec7e495d64
Dhuha Hasan <duha_9@yahoo.com>
2019-10-29 08:40:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ let tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials.map(oneStr=>oneStr.)
+  return tutorials.map(oneStr=>oneStr
   .toLowerCase()
   .split(" ")
   .map(word=>word.charAt(0).toUpperCase()+word.slice(1))
   .join(" ");
-};
+});
